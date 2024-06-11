91db334081691f1b94198e8195649bdf6ba8bb3a
hanchi <hanchi@tosky.co.kr>
2024-06-11 14:00:48 +0900

    feat: 로그아웃 버튼 변경 및 텍스트 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import { Button } from "../ui/button";
 
 type Props = {};
 
@@ -9,11 +10,13 @@ export default function Header({}: Props) {
   const { data: session } = useSession();
 
   return (
-    <div>
+    <div className="flex justify-end items-center p-3 gap-2">
       {session && (
         <>
           <h1>안녕하세요, {session.user?.name}님</h1>
-          <button onClick={() => signOut()}>Sign out</button>
+          <Button onClick={() => signOut()} className="">
+            로그아웃
+          </Button>
         </>
       )}
     </div>
